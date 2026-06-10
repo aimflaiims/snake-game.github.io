a4dadec70e820d6ecb69621c9e9dccc015a88b3c
ROHIT DHIMAN <rd00829849@techmahindra.com>
2026-06-10 18:58:46 +0530

    Updates

---
+++ precache-manifest.2c0bc42b99f6baf609c6656ecb74233a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42699636c436743f8959",
+    "url": "/snake-game.github.io/static/css/main.78cbc03c.chunk.css"
+  },
+  {
+    "revision": "42699636c436743f8959",
+    "url": "/snake-game.github.io/static/js/main.42699636.chunk.js"
+  },
+  {
+    "revision": "23f7706b56b0ee0a61f0",
+    "url": "/snake-game.github.io/static/css/1.375ccc41.chunk.css"
+  },
+  {
+    "revision": "23f7706b56b0ee0a61f0",
+    "url": "/snake-game.github.io/static/js/1.23f7706b.chunk.js"
+  },
+  {
+    "revision": "d1a6d7869df9beec4670",
+    "url": "/snake-game.github.io/static/js/runtime~main.d1a6d786.js"
+  },
+  {
+    "revision": "d4db2871b1bfabf26cebbf5907b7f838",
+    "url": "/snake-game.github.io/index.html"
+  }
+];

c4f15fc0b2bc171b46b1b42c10fe0d1435e11962
ROHIT DHIMAN <rd00829849@techmahindra.com>
2026-06-10 19:06:20 +0530

    Fix GitHub Pages routing and Node compatibility
    - switch BrowserRouter to HashRouter for refresh-safe routes
    
    - add Node/OpenSSL legacy provider support for CRA scripts
    
    - add .nvmrc for reproducible local runtime

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
+import Login from "./components/Login";
+import Error404 from "./components/Error404";
+import Navigation from './components/Navigation';
+import Ground from './components/Snake';
+
+const Home = () => {
+  return (
+    <div>
+      <Ground size={20}/>
+    </div>
+  );
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedIn: false,
+      username: null,
+      user_id: null,
+    };
+
+    this.login = this.login.bind(this);
+  }
+
+  login(data) {
+    this.setState({
+      loggedIn: true,
+      username: data.username,
+      user_id: data.user_id,
+    });
+  }
+
+  render() {
+    return (
+      <HashRouter>
+        <div className="container">
+          <Navigation />
+          <hr/>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/login" component={Login} login={this.login} />
+            <Route component={Error404} />
+          </Switch>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
